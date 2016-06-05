e4ed027c78a507a47588403a6ea1a19cfa507de9
Brian Maxwell <brianmaxwell1@gmail.com>
2016-06-05 12:33:41 -0400

    refactor json rendering in facebook_login_controller

--- web/controllers/facebook_login_controller.ex
+++ web/controllers/facebook_login_controller.ex
@@ -19,42 +19,42 @@ defmodule Skiptip.FacebookLoginController do
   end
 
   def callback(conn, params) do
+    HTTPoison.start
     app_id = "504019469804454"
     app_secret = "3c071367622fc724dd500705d659170c"
     code = params["code"]
     callback_url = facebook_login_url(conn, :callback)
-    access_token_url = "https://graph.facebook.com/v2.3/oauth/access_token?"
-    access_token_url = access_token_url <> "client_id=" <> app_id <> "&"
-    access_token_url = access_token_url <> "client_secret=" <> app_secret <> "&"
-    access_token_url = access_token_url <> "code=" <> code <> "&"
-    access_token_url = access_token_url <> "redirect_uri=" <> callback_url
-    HTTPoison.start
-    case HTTPoison.get(access_token_url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        token = Poison.decode!(body)["access_token"]
-        user_id = get_facebook_user_id_from_access_token(token)
-        login_or_create_user(token, user_id)
-        render(conn, "callback.json", response: [token, user_id])
-      {:ok, %HTTPoison.Response{status_code: 400, body: body}} ->
-        #error = Poison.encode!(Poison.decode!(body)["error"])
-        render(conn, "callback.json", response: body)
-    end
+    access_token_url = "https://graph.facebook.com/v2.3/oauth/access_token"
+    parameters = %{client_id: app_id, client_secret: app_secret, code: code, redirect_uri: callback_url}
+    get_request = HTTPoison.get!(access_token_url, [], params: parameters)
+    %HTTPoison.Response{body: body, status_code: status_code} = get_request
+    render_callback(conn, status_code, body)
   end
 
-  def get_facebook_user_id_from_access_token(token) do
-    user_id_url = "https://graph.facebook.com/me?fields=id&access_token=" <> token
+  def render_callback(conn, 200, body) do
+    token = Poison.decode!(body)["access_token"]
+    user_id = get_facebook_user_id(token)
+    login_or_create_user(token, user_id)
+    response = %{token: token, user_id: user_id}
+    render(conn, "callback.json", response: response)
+  end
+  
+  def render_callback(conn, 400, body) do
+    body = Poison.decode! body
+    render(conn, "callback.json", response: body)
+  end
+
+  def get_facebook_user_id(access_token) do
+    user_id_url = "https://graph.facebook.com/me?fields=id&access_token=" <> access_token
     HTTPoison.start
-    case HTTPoison.get(user_id_url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        Poison.decode!(body)["id"]
-    end
+    %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(user_id_url)
+    Poison.decode!(body)["id"]
   end
 
   def login_or_create_user(facebook_access_token, facebook_user_id) do
     unless FacebookLogin.find_by(:facebook_user_id, facebook_user_id) do
       User.create(facebook_access_token, facebook_user_id)
     end
-    #login(token, user_id
   end
 
 end
--- web/views/facebook_login_view.ex
+++ web/views/facebook_login_view.ex
@@ -1,4 +1,12 @@
 defmodule Skiptip.FacebookLoginView do
   use Skiptip.Web, :view
+  
+  def render("callback.json", %{response: response}) do
+    response
+  end
+
+  def render("callback.json", %{error: error}) do
+    error
+  end
 
 end
